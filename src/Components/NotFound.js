3b96f0b20096d35596b4f48ef7d0c969e7dc2079
robertpm <grobertpm@gmail.com>
2022-02-24 01:37:12 -0800

    Updated notfound page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -8,9 +8,9 @@ const NotFound = () => {
 	return (
 		<div className="not-found">
 
-			<h2 className="top-heading not-found-heading">404</h2>
+			<h2 className="top-heading not-found-heading">404 — Page Not Found</h2>
 
-			<p><strong>{`Unfortunately, the page you're looking for doesn't exist.`}</strong></p>
+			<p><strong>{`Unfortunately, the page you're looking for doesn't exist.`}<br /><br />Live long and prosper.🖖</strong></p>
 
 			<img className="my-logo" src={ myLogo } alt="My Logo" />
       
